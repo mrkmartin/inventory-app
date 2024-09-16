b2caa0ae9e7399643f6173474422d8ef0d23d2bb
Mark <jetblackzero04@gmail.com>
2024-09-16 22:13:39 +0800

    Add function for adding product and add text input

--- api.ts
+++ api.ts
@@ -1,9 +1,22 @@
-import { Products } from './types/products';
+import { Products } from "./types/products";
 
-const API_URL = 'http://localhost:3001/';
+const API_URL = "http://localhost:3001/";
 
 export const fetchInventory = async (): Promise<Products[]> => {
   const response = await fetch(`${API_URL}/products`);
   const items = await response.json();
   return items;
+};
+
+export const addProduct = async (product: Products): Promise<Products> => {
+  const res = await fetch(`${API_URL}/products`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(product),
+  });
+
+  const newProduct = await res.json();
+  return newProduct;
 };
--- app/components/AddItem.tsx
+++ app/components/AddItem.tsx
@@ -1,49 +1,82 @@
-'use client';
+"use client";
 
-import { AiOutlinePlus } from 'react-icons/ai';
-import ProductForm from './ProductForm';
-import { useState } from 'react';
+import { AiOutlinePlus } from "react-icons/ai";
+import ProductForm from "./ProductForm";
+import { FormEventHandler, useState } from "react";
+import { addProduct } from "@/api";
 
 const AddItem = () => {
   const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [newProductName, setNewProductName] = useState<string>("");
+  const [newQuantity, setNewQuantity] = useState(0);
+  const [newPrice, setNewPrice] = useState(0);
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await addProduct({
+      id: "3",
+      "product name": newProductName,
+      quantity: 100000,
+      price: 900,
+    });
+    setNewProductName("");
+    setNewQuantity(0);
+    setNewPrice(0);
+  };
+
   return (
     <div>
       <button
         onClick={() => setModalOpen(true)}
-        className='btn btn-primary w-full uppercase font-bold'
+        className="btn btn-primary w-full uppercase font-bold"
       >
-        Add New Item <AiOutlinePlus className='ml-2' size={18} />
+        Add New Item <AiOutlinePlus className="ml-2" size={18} />
       </button>
 
       <ProductForm modalOpen={modalOpen} setModalOpen={setModalOpen}>
-        <form>
-          <h3 className='font-bold text-lg'>Add New Product</h3>
-          <div className='modal-action flex flex-col'>
-            <label>
-              Product Name
+        <form onSubmit={handleSubmit}>
+          <h3 className="font-bold text-lg">Add New Product</h3>
+          <div className="modal-action flex flex-col max-w-xs px-3 py-4 gap-5 mx-auto">
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Product Name</span>
+              </div>
               <input
-                type='text'
-                placeholder='Product Name'
-                className='input input-bordered w-full max-w-xs flex self-end'
+                value={newProductName}
+                onChange={(e) => setNewProductName(e.target.value)}
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
               />
             </label>
-
-            <input
-              type='text'
-              placeholder='Quantity'
-              className='input input-bordered w-full max-w-xs'
-            />
-            <input
-              type='text'
-              placeholder='Price'
-              className='input input-bordered w-full max-w-xs'
-            />
-            <input
-              type='text'
-              placeholder='Type here'
-              className='input input-bordered w-full max-w-xs'
-            />
-            <button type='submit' className='btn'>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Quantity</span>
+              </div>
+              <input
+                value={newQuantity}
+                onChange={(e) => setNewQuantity(Number(e.target.value))}
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Price</span>
+              </div>
+              <input
+                value={newPrice}
+                onChange={(e) => setNewPrice(Number(e.target.value))}
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+            <button
+              type="submit"
+              className="btn btn-success text-white uppercase"
+            >
               Submit
             </button>
           </div>
