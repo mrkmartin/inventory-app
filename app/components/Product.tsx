75530e042f162bf2ce940ab3d5fcea9714dfd4dd
Mark <jetblackzero04@gmail.com>
2024-09-17 22:16:42 +0800

    Add edit and delete icon

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Products } from '@/types/products';
+import React from "react";
+import { Products } from "@/types/products";
+import { FiEdit, FiTrash2 } from "react-icons/fi";
 
 interface ProductProps {
   product: Products;
@@ -9,9 +10,11 @@ const Product: React.FC<ProductProps> = ({ product }) => {
   return (
     <tr key={product.id}>
       <th>{product.id}</th>
-      <td>{product['product name']}</td>
+      <td>{product["product name"]}</td>
       <td>{product.quantity}</td>
       <td>{`\$${product.price}`}</td>
+      <FiEdit size={25} />
+      <FiTrash2 />
     </tr>
   );
 };
