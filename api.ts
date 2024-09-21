500f343aeb0c6073f4417d40f2f8b297a43fc96d
mrkmartin <markpaulomartin@gmail.com>
2024-09-21 15:56:54 +0800

    Add error handling

--- api.ts
+++ api.ts
@@ -2,40 +2,63 @@ import { Products } from './types/products';
 
 const API_URL = 'http://localhost:3001/products';
 
+const fetchOptions = (method: string, body?: any) => ({
+  method,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: body ? JSON.stringify(body) : null,
+});
+
+// Fetch all products
 export const fetchInventory = async (): Promise<Products[]> => {
-  const response = await fetch(API_URL, { cache: 'no-store' });
-  const items = await response.json();
-  return items;
+  try {
+    const response = await fetch(API_URL, { cache: 'no-store' });
+    if (!response.ok) throw new Error('Failed to fetch inventory');
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching inventory:', error);
+    throw error;
+  }
 };
 
+// Add a new product
 export const addProduct = async (product: Products): Promise<Products> => {
-  const res = await fetch(API_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(product),
-  });
+  try {
+    const res = await fetch(API_URL, fetchOptions('POST', product));
+    if (!res.ok) throw new Error('Failed to add product');
 
-  const newProduct = await res.json();
-  return newProduct;
+    return await res.json();
+  } catch (error) {
+    console.error('Error adding product:', error);
+    throw error;
+  }
 };
 
+// Edit an existing product
 export const editProduct = async (product: Products): Promise<Products> => {
-  const res = await fetch(`${API_URL}/${product.id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(product),
-  });
+  try {
+    const res = await fetch(
+      `${API_URL}/${product.id}`,
+      fetchOptions('PUT', product)
+    );
+    if (!res.ok) throw new Error('Failed to update product');
 
-  const updatedProduct = await res.json();
-  return updatedProduct;
+    return await res.json();
+  } catch (error) {
+    console.error('Error updating product:', error);
+    throw error;
+  }
 };
 
+// Delete a product by ID
 export const deleteProduct = async (id: string): Promise<void> => {
-  await fetch(`${API_URL}/${id}`, {
-    method: 'DELETE',
-  });
+  try {
+    const res = await fetch(`${API_URL}/${id}`, fetchOptions('DELETE'));
+    if (!res.ok) throw new Error('Failed to delete product');
+  } catch (error) {
+    console.error('Error deleting product:', error);
+    throw error;
+  }
 };
