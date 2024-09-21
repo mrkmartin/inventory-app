b66d4e1ad403b55835e2169b3eba245263e036ae
mrkmartin <markpaulomartin@gmail.com>
2024-09-21 16:04:39 +0800

    Adjust the design

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   return (
     <main className='max-w-4xl mx-auto mt-4'>
       <div className='text-center my-5 flex flex-col gap-4'>
-        <h1 className='text-2xl font-bold'>Inventory App</h1>
+        <h1 className='text-3xl font-bold'>Inventory App</h1>
         <AddItem />
       </div>
       <InventoryList products={products} />
